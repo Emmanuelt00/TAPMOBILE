da4067c9ccb7fa7695bfa57352427420de4c4f86
Emmanuelt00 <98651931+Emmanuelt00@users.noreply.github.com>
2023-12-24 15:42:43 -0500

    TAPMOBILEv1.1
    This is an updated version to my TAP mobile & PC game which at first contained errors in which the player couldn't tap inside of the circle because it was missing a 2D circle collider.

--- Assets/TapMobileGame/2DCircleBehavior.cs
+++ Assets/TapMobileGame/2DCircleBehavior.cs
@@ -1,55 +1,94 @@
+using System.Collections;
 using UnityEngine;
 
 public class CircleBehavior : MonoBehaviour
 {
-    public GameObject circle; // Reference to the circle GameObject
-    public float moveArea = 5f;  // The area within which the circle can move
-    public float moveInterval = 5f;  // Time interval for moving the circle
-    public float colorChangeSpeed = 0.5f; // Speed at which the color changes
-    public Vector2 spawnArea = new Vector2(10, 10); // Area within which the circle will spawn
+    public ScoreManager scoreManager; // Reference to the ScoreManager script
+    public float moveArea = 5f;
+    public float moveInterval = 5f;
+    public float colorChangeSpeed = 0.5f;
+
     private Vector3 startPosition;
     private Vector3 targetPosition;
     private float timer;
     private SpriteRenderer spriteRenderer;
+    private Collider2D circleCollider;
 
-    private void Start()
+    void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-
-        // Store the initial position of the circle
         startPosition = transform.position;
-
-        // Initialize the timer
         timer = moveInterval;
-
-        // Start by staying in the initial position
         targetPosition = startPosition;
+
+        // Attempt to get the Collider2D component
+        circleCollider = GetComponent<Collider2D>();
+
+        // Check if the Collider2D component is not found
+        if (circleCollider == null)
+        {
+            Debug.LogError("Collider2D component not found on the object.");
+        }
+
+        // Find and store the ScoreManager script
+        scoreManager = FindObjectOfType<ScoreManager>();
+
+        // Check if the ScoreManager script is found
+        if (scoreManager == null)
+        {
+            Debug.LogError("ScoreManager script not found in the scene.");
+        }
     }
 
-    private void Update()
+    void Update()
     {
-        // Update the timer
         timer -= Time.deltaTime;
 
         if (timer <= 0f)
         {
-            // Set a new random target position
             SetNewRandomTarget();
-
-            // Reset the timer
             timer = moveInterval;
         }
 
-        // Move the circle towards the target position
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * moveArea);
         Color rainbowColor = Color.HSVToRGB(Mathf.PingPong(Time.time * colorChangeSpeed, 1f), 1f, 1f);
         spriteRenderer.color = rainbowColor;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckMouseClick();
+        }
     }
 
-    private void SetNewRandomTarget()
+    void SetNewRandomTarget()
     {
         float randomX = Random.Range(-moveArea, moveArea);
         float randomY = Random.Range(-moveArea, moveArea);
         targetPosition = startPosition + new Vector3(randomX, randomY, 0f);
     }
+
+    void CheckMouseClick()
+    {
+        if (circleCollider != null)
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0f;
+
+            if (circleCollider.bounds.Contains(mousePosition))
+            {
+                // Increment the score by 1 when clicking inside the circle
+                scoreManager.IncrementScore();
+            }
+            else
+            {
+                // Decrement the score by 1 when clicking outside the circle
+                scoreManager.DecrementScore();
+            }
+        }
+        else
+        {
+            Debug.LogError("Collider2D component is null.");
+        }
+    }
 }
+
--- Assets/TapMobileGame/SCORECOUNT.cs
+++ Assets/TapMobileGame/SCORECOUNT.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
@@ -7,7 +8,6 @@ public class ScoreManager : MonoBehaviour
 {
     public Text scoreText; // Reference to the UI Text element for displaying the score
     private int score = 0;
-    private int maxScore = 100; // The maximum score to reach
 
     void Start()
     {
@@ -15,37 +15,27 @@ public class ScoreManager : MonoBehaviour
         UpdateScoreText();
     }
 
-    void Update()
-    {
-        // Check for mouse click or tap on a touch screen
-        if (Input.GetMouseButtonDown(0) && score < maxScore)
-        {
-            // Increment the score by 1
-            score++;
-
-            // Update the UI text to display the current score
-            UpdateScoreText();
-
-            // Check if the maximum score is reached
-            if (score >= maxScore)
-            {
-                // Add your exit logic here (e.g., Application.Quit())
-                Debug.Log("Score reached 100. Exiting the program.");
-                Application.Quit();
-                UnityEditor.EditorApplication.isPlaying = false; // This exits play mode in the Unity Editor
-            }
-        }
-    }
-
     void UpdateScoreText()
     {
-        // Update the UI text to display "SCORE = X" where X is the current score
-        scoreText.text = "SCORE = " + score.ToString();
-        // Change the text color to yellow
-        scoreText.color = Color.black;
+        // Update the UI text to display "Score: X" where X is the current score
+        scoreText.text = "Score: " + score;
+    }
 
-        // Set the font style to bold and change the font size
-        scoreText.fontStyle = FontStyle.Bold;
-        scoreText.fontSize = 80; // Adjust the font size as needed
+    public void IncrementScore()
+    {
+        // Increment the score by 1
+        score++;
+
+        // Update the UI text to display the current score
+        UpdateScoreText();
+    }
+
+    public void DecrementScore()
+    {
+        // Decrement the score by 1
+        score--;
+
+        // Update the UI text to display the current score
+        UpdateScoreText();
     }
 }
